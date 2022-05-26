48e7685ae36b851dd50669599905c0a986d537bb
Kovacs Dorina <kovacsdorina2@gmail.com>
2022-05-26 09:27:15 +0200

    Kezdes

---
+++ src/szamit.php
@@ -0,0 +1,36 @@
+
+
+<?php
+
+// echo file_get_contents('templates/head.html');
+
+// $page = file_get_contents('templates/szamit.html');
+
+
+
+// function Szamitas($aTengely, $bTengely){
+
+//     $terF = (4*M_PI)/3*(pow($aTengely,2)*$bTengely);
+    
+
+//     return $terF;
+// }
+
+//     if(!empty($_GET['aTengely']) and !empty($_GET['bTengely'])) 
+//     {
+//         $aTengely = $_GET['aTengely'];
+//         $bTengely = $_GET['bTengely'];
+
+//         $terF= Szamitas($aTengely, $bTengely);
+//     }
+//     else{
+//         $terF = "HIBA!";
+//         }
+
+//     $terF= Szamitas($aTengely, $bTengely);
+
+//     $page= str_replace('{{eredmeny}}', $terF, $page);
+
+//     echo $page;
+//     echo file_get_contents('templates/foot.html');
+?> 
